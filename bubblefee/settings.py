9a035300b32517750f464fd9940134cfa5ea1051
Alice Chan <alicebay10@yahoo.co.uk>
2024-09-30 08:55:29 +0000

    changed code to SECRET KEY.

--- bubblefee/settings.py
+++ bubblefee/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5y08(sc#7q68jt5jx7k6jz#sl+-&x7v%%6#%)#neg#g+!boa@c'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
